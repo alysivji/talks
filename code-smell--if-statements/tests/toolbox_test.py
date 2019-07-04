a362bdce23c058fb768fb00b75c717b7764551f9
Aly Sivji <alysivji@gmail.com>
2019-07-04 12:03:02 -0500

    add raise for exception test

--- code-smell--if-statements/tests/toolbox_test.py
+++ code-smell--if-statements/tests/toolbox_test.py
@@ -1,4 +1,7 @@
 import pytest
+import responses
+from requests.exceptions import HTTPError
+
 from github_summary.toolbox import get_github_events
 
 
@@ -7,3 +10,15 @@ from github_summary.toolbox import get_github_events
 def test_get_github_events():
     events = get_github_events("alysivji")
     assert len(events) > 0
+
+
+@responses.activate
+def test_get_github_events_returns_400():
+    responses.add(
+        responses.GET,
+        "https://api.github.com/users/alysivji/events/public",
+        status=400,
+    )
+
+    with pytest.raises(HTTPError):
+        get_github_events("alysivji")
