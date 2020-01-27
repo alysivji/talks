80d1f58c6e839251db767dfddac361945888368e
Aly Sivji <alysivji@gmail.com>
2020-01-26 18:17:56 -0600

    encapsulate in functions

---
+++ design-patterns--facade-patterns/changelog/b_adding_functions.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+
+import requests
+
+GITHUB_OAUTH_TOKEN = os.getenv("GITHUB_OAUTH_TOKEN", None)
+
+
+def generate_changelog(owner, repo, version):
+    BASE_URL = f"https://api.github.com/repos/{owner}/{repo}"
+    release_dt = get_release_date(BASE_URL, version)
+    return get_commit_messages(BASE_URL, release_dt)
+
+
+def get_release_date(base_url, version):
+    resp = requests.get(f"{base_url}/releases/tags/{version}")
+    if resp.status_code == 404:
+        raise ValueError("Version does not exist")
+    resp.raise_for_status()
+    return resp.json()["published_at"]
+
+
+def get_commit_messages(base_url, release_dt):
+    params = {"sha": "master", "since": release_dt}
+    resp = requests.get(f"{base_url}/commits", params=params)
+    resp.raise_for_status()
+    messages = [item.get("commit", {}).get("message") for item in resp.json()]
+    return messages[::-1]
+
+
+def parse_args():
+    description = "Generate changelog for repository"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-r",
+        "--repo",
+        type=str,
+        help="Full path to repository, (abc/xyz)",
+        required=True,
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        help="Version to generate CHANGELOG from",
+        required=True,
+    )
+    return vars(parser.parse_args())
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    try:
+        owner, repo = args["repo"].split("/")
+    except ValueError:
+        raise ValueError("Invalid repo")
+    version = args["version"]
+
+    changelog = generate_changelog(owner, repo, version)
+    print()
+    print("\n".join(changelog))
---
+++ design-patterns--facade-patterns/changelog/tests/b_adding_functions_test.py
@@ -0,0 +1,51 @@
+import pytest
+import responses
+
+from changelog.b_adding_functions import (
+    generate_changelog,
+    get_release_date,
+    get_commit_messages,
+)
+
+
+@responses.activate
+@pytest.mark.unit
+def test_get_release_date():
+    responses.add(
+        responses.GET,
+        "http://url/releases/tags/1.0.0",
+        json={"published_at": "2020-01-26"},
+    )
+
+    release_dt = get_release_date("http://url", "1.0.0")
+
+    assert release_dt == "2020-01-26"
+
+
+@responses.activate
+@pytest.mark.unit
+def test_get_commits():
+    responses.add(
+        responses.GET,
+        "http://url/commits",
+        json=[
+            {"commit": {"message": "last commit"}},
+            {"commit": {"message": "first commit"}},
+        ],
+    )
+    messages = get_commit_messages("http://url", release_dt="value")
+
+    assert messages == ["first commit", "last commit"]
+
+
+@responses.activate
+@pytest.mark.unit
+def test_generate_changelog(mocker):
+
+    mocker.patch("changelog.b_adding_functions.get_release_date")
+    commit_mock = mocker.patch("changelog.b_adding_functions.get_commit_messages")
+    commit_mock.return_value = ["first commit", "last commit"]
+
+    messages = generate_changelog("owner", "repo", "1.0.0")
+
+    assert messages == ["first commit", "last commit"]
