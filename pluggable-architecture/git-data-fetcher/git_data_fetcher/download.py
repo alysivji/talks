affbd9b4b45d778b842e1c59fa357490e1f15968
Aly Sivji <alysivji@gmail.com>
2020-05-24 10:50:36 -0500

    restructure code

--- pluggable-architecture/git-data-fetcher/git_data_fetcher/download.py
+++ pluggable-architecture/git-data-fetcher/git_data_fetcher/download.py
@@ -3,24 +3,20 @@ from urllib.parse import urlparse
 
 
 class RepoDetails(NamedTuple):
-    domain: str
     organization: str
     repo: str
 
 
 class GitFetcher:
-    def __init__(self, url, external: List = None):
-        self.repo = self._parse_url(url)
-        if not external:
-            external = []
-        self.external = external
+    providers = []
 
-    def get_commits(self):
-        pass
+    def __init__(self, url, external: List = None):
+        domain, self.repo = self._parse_url(url)
 
     def _parse_url(self, url):
         url_parts = urlparse(url)
         parts = url_parts.path.split("/")
-        return RepoDetails(
-            domain=url_parts.netloc, organization=parts[1], repo=parts[2]
-        )
+        return url_parts.netloc, RepoDetails(organization=parts[1], repo=parts[2])
+
+    def get_commits(self):
+        pass
