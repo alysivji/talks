3fb26e0a59e6caa0c12858564a51ae7b0dc95bd7
Aly Sivji <alysivji@gmail.com>
2020-05-24 12:23:31 -0500

    use vcr.py to stub tests

--- pluggable-architecture/git-data-fetcher/tests/providers/test_gitlab.py
+++ pluggable-architecture/git-data-fetcher/tests/providers/test_gitlab.py
@@ -1,7 +1,10 @@
+import pytest
+
 from git_data_fetcher.download import RepoDetails
 from git_data_fetcher.providers.gitlab_provider import GitLabProvider
 
 
+@pytest.mark.vcr()
 def test_gitlab_provider():
     repo = RepoDetails(organization="pycqa", repo="flake8")
     client = GitLabProvider(repo)
