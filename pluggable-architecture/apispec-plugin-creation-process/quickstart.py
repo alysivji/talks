f4d11785aa39a60fd1d445aac4f590e143ec939c
Aly Sivji <alysivji@gmail.com>
2020-06-18 14:37:24 -0500

    get quickstart working

---
+++ pluggable-architecture/apispec-plugin-creation-process/quickstart.py
@@ -0,0 +1,61 @@
+import json
+import uuid
+
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from flask import Flask
+from marshmallow import Schema, fields
+
+
+# Create an APISpec
+spec = APISpec(
+    title="Swagger Petstore",
+    version="1.0.0",
+    openapi_version="3.0.2",
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
+
+# Optional marshmallow support
+class CategorySchema(Schema):
+    id = fields.Int()
+    name = fields.Str(required=True)
+
+
+class PetSchema(Schema):
+    categories = fields.List(fields.Nested(CategorySchema))
+    name = fields.Str()
+
+
+# Optional Flask support
+app = Flask(__name__)
+
+
+@app.route("/random")
+def random_pet():
+    """A cute furry animal endpoint.
+    ---
+    get:
+      description: Get a random pet
+      responses:
+        200:
+          description: Return a pet
+          content:
+            application/json:
+              schema: PetSchema
+    """
+    # Hardcoded example data
+    pet_data = {
+        "name": "sample_pet_" + str(uuid.uuid1()),
+        "categories": [{"id": 1, "name": "sample_category"}],
+    }
+    return PetSchema().dump(pet_data)
+
+
+# Register the path and the entities within it
+with app.test_request_context():
+    spec.path(view=random_pet)
+
+
+# Print JSON
+print(json.dumps(spec.to_dict(), indent=2))
